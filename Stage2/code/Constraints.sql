e141f1fd901b0825eb21d11a0a79678d629f3d55
AriellaLea <ariellaatlan@gmail.com>
2026-05-11 09:33:03 +0300

    Add constraints for positive values in tables
    Added constraints to ensure positive values for points_balance, price, and employee_count. Included test fail cases for validation.

--- Stage2/code/Constraints.sql
+++ Stage2/code/Constraints.sql
@@ -8,7 +8,7 @@ ALTER TABLE customers
 ADD CONSTRAINT chk_points_balance_positive 
 CHECK (points_balance >= 0);
 
--- TEST FAIL (Capture this error for your report):
+-- TEST FAIL :
 -- INSERT INTO customers (customer_id, name, email, points_balance, loyalty_level_id) 
 -- VALUES (9999, 'Test User', 'test@example.com', -10, 1);
 
@@ -18,7 +18,7 @@ ALTER TABLE products
 ADD CONSTRAINT chk_product_price_positive 
 CHECK (price > 0);
 
--- TEST FAIL (Capture this error for your report):
+-- TEST FAIL :
 -- INSERT INTO products (product_id, product_name, price, stock_quantity, category_id) 
 -- VALUES (8888, 'Broken Product', -5.00, 10, 1);
 
@@ -28,6 +28,6 @@ ALTER TABLE marketing_management
 ADD CONSTRAINT chk_management_employee_min 
 CHECK (employee_count > 0);
 
--- TEST FAIL (Capture this error for your report):
+-- TEST FAIL :
 -- INSERT INTO marketing_management (director_id, strategy_type_id, employee_count) 
 -- VALUES (7777, 1, 0);
